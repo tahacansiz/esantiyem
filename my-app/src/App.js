8d6e71b4ec7b6a39dea6d0d16a32bf155874eefa
tahacansiz <etcansiz@gmail.com>
2025-06-30 00:29:46 +0300

    Implement basic marketplace layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react';
+import './Marketplace.css';
+import Navbar from './components/Navbar';
+import SearchSection from './components/SearchSection';
+import CategoryGrid from './components/CategoryGrid';
+import ListingGrid from './components/ListingGrid';
+import AuthForms from './components/AuthForms';
 
 function App() {
   return (
-    
-    helo
+    <div className="app">
+      <Navbar />
+      <div className="container">
+        <SearchSection />
+        <CategoryGrid />
+        <ListingGrid />
+        <AuthForms />
+      </div>
+    </div>
   );
 }
 
